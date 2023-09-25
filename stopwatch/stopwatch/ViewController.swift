827ec6b3a630bf7f1c87c3d3ba5ef66a9d126a2c
heejinnn <gml0593@gmail.com>
2023-09-25 11:03:34 +0900

    upload: stopwatch project

---
+++ stopwatch/stopwatch/ViewController.swift
@@ -0,0 +1,39 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var pauseBtn: UIButton!
+    @IBOutlet weak var resetBtn: UIButton!
+   
+    var timer : Timer?
+    var timerNum: Float = 0.0
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        playBtn.addTarget(self, action: #selector(startTimer), for: .touchUpInside)
+        pauseBtn.addTarget(self, action: #selector(pauseTimer), for: .touchUpInside)
+        resetBtn.addTarget(self, action: #selector(resetTimer), for: .touchUpInside)
+    }
+    
+    @objc func startTimer(){
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ timer in
+            self.timerNum += 0.1
+            self.numberLabel.text = "\(self.timerNum)"
+        }
+    }
+    
+    @objc func pauseTimer(){
+        timer?.invalidate()
+    }
+    
+    @objc func resetTimer(){
+        self.timerNum = 0.0
+        self.numberLabel.text = "\(self.timerNum)"
+    }
+    
+}
+
