10987934a42ae3e3c1642bde1dc5a2998128d50a
heejinnn <gml0593@gmail.com>
2024-01-09 12:45:08 +0900

    upload: ImageSearch

---
+++ ImageSearch/ImageSearch/Data.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+struct Data {
+    var id : Int
+    let name : String
+    let image: UIImage
+}
+
+extension Data{
+    static let list: [Data] = [
+        Data(id: 1, name: "apple", image: UIImage(named: "apple")!),
+       Data(id: 2, name: "grape", image: UIImage(named: "grape")!),
+       Data(id: 3, name: "mango", image: UIImage(named: "mango")!)
+    ]
+}
---
+++ ImageSearch/ImageSearch/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  ImageSearch
+//
+//  Created by 최희진 on 2024/01/09.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+    
+    let viewTitle  = UILabel()
+    let imageSearchTextField = UITextField()
+    let imageView = UIImageView()
+    let separatorView = UIView()
+    
+    let imagedata = Data.list
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        initView()
+        imageSearchTextField.delegate = self
+    }
+    
+    func initView(){
+        view.addSubview(viewTitle)
+        view.addSubview(imageSearchTextField)
+        view.addSubview(imageView)
+        view.addSubview(separatorView)
+        
+        imageSearchTextField.placeholder = "Enter the name"
+        imageSearchTextField.textAlignment = .center
+        
+        
+        viewTitle.text = "WIKIFRUIT"
+        viewTitle.font = .boldSystemFont(ofSize: 35)
+        
+        separatorView.backgroundColor = .black
+        
+        
+        viewTitle.snp.makeConstraints{ make in
+            make.top.equalToSuperview().offset(80)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(40)
+        }
+        
+        imageSearchTextField.snp.makeConstraints{make in
+            make.top.equalTo(viewTitle.snp.bottom).offset(40)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(40)
+        }
+        
+        imageView.snp.makeConstraints{make in
+            make.top.equalTo(imageSearchTextField.snp.bottom).offset(60)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(350)
+            make.height.equalTo(500)
+        }
+        
+        separatorView.snp.makeConstraints { make in
+            make.top.equalTo(imageSearchTextField.snp.bottom).offset(5)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(300)
+            make.height.equalTo(1)
+        }
+    }
+
+}
+
+extension ViewController: UITextFieldDelegate{
+    
+    //enter 키 누를 때
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()//enter 키 누를 때 키보드 닫기
+        
+        if let enteredText = textField.text?.lowercased() {
+            if let imageData = imagedata.first(where: { $0.name.lowercased() == enteredText }) {
+                imageView.image = imageData.image
+            } else {
+                imageView.image = nil
+            }
+        }
+        
+        return true
+    }
+}
