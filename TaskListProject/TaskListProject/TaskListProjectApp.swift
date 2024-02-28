36c168d870b6be8979dd3e5a3a5d1f315202a3c3
heejinnn <gml0593@gmail.com>
2024-02-28 23:48:08 +0900

    upload: Task Main View ui 구현

---
+++ TaskListProject/TaskListProject/TaskListProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListProjectApp.swift
+//  TaskListProject
+//
+//  Created by 최희진 on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskListProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
